35681cab4725025a1d209ac4a977d4c8b4448c57
Mihai Ureche <ureche.mihai@gmail.com>
2022-04-15 11:08:03 +0300

    Update NewsService.js

--- src/services/NewsService.js
+++ src/services/NewsService.js
@@ -2,13 +2,13 @@ class NewsService {
     baseApiUrl = 'http://localhost:3000/';
 
     getNews = () => {
-        return fetch(this.baseApiUrl+'v2/headlines')
+        return fetch(this.baseApiUrl+'headlines')
         .then((res) => res.json())
         .then((results) => results.art)
     }
 
     searchNews = (searchText) => {
-        return fetch(this.baseApiUrl+`v2/search/country/RO/text/${searchText}`)
+        return fetch(this.baseApiUrl+`search/${searchText}`)
         .then((res) => res.json())
         .then((results) => results.articles)
     }
